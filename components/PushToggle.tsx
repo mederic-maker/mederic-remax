bb4e3bc30ba7e658cb9282c71889fe9e009be015
mederic souccar <medericsouccar@mederics-MacBook-Pro.local>
2026-05-01 16:21:17 -0400

    Fix PushToggle types

--- components/PushToggle.tsx
+++ components/PushToggle.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 import { Bell, BellOff } from 'lucide-react'
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string) {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
